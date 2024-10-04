4ae39dcac2f0eb44e91e94b1dda1f72a0829d8fa
hitochix06 <pachara.philibert@hotmail.fr>
2024-10-04 11:17:36 +0200

    Refactor NavBar component to update language dropdown options

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,15 +10,10 @@ function navigation() {
     <Navbar.Brand href="#home"><img src={logo} alt="icon" style={{ width: '30px', marginRight: '8px' }} /> Pokedex<span className='red-text' >ID</span></Navbar.Brand>
     <Navbar.Toggle />
     <Navbar.Collapse className="justify-content-end ">
-     <NavDropdown title="Langue" id="basic-nav-dropdown" className='couleur-text'>
-      <NavDropdown.Item href="#action/3.1">Fr</NavDropdown.Item>
+     <NavDropdown title="Fr" id="basic-nav-dropdown" className='couleur-text'>
+      <NavDropdown.Item href="#action/3.1"> EN</NavDropdown.Item>
       <NavDropdown.Item href="#action/3.2">
-       Another action
-      </NavDropdown.Item>
-      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-      <NavDropdown.Divider />
-      <NavDropdown.Item href="#action/3.4">
-       Separated link
+       CH
       </NavDropdown.Item>
      </NavDropdown>
     </Navbar.Collapse>
