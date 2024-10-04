cb919800f6816d78b4c477336d1dbe9e31545e32
hitochix06 <pachara.philibert@hotmail.fr>
2024-10-04 11:14:15 +0200

    Refactor code to remove unused CSS and import statements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,25 @@
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
-import { Row }  from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import pokemon from '../assets/pokemon/1.png';
 
 
 function Cards() {
   return (
     <Row>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>nom pokemon</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Badge bg="primary" className='me-2'  >Type</Badge>
-            <Badge bg="primary" className='me-2'>Type</Badge>
-          </Card.Body>
-        </Card>
-        
+      <Card style={{ width: '18rem' }}>
+        <Card.Body className="text-center">
+          <Card.Title>nom pokemon</Card.Title>
+          <Card.Img variant="top" src={pokemon} style={{ width: '100px' }} />
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Badge bg="primary" className='me-2'  >Type</Badge>
+          <Badge bg="primary" className='me-2'>Type</Badge>
+        </Card.Body>
+      </Card>
+
     </Row>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../pokeball.png';
 
 function navigation() {
  return (
-  <Navbar className='fond-red'>
-
+  <Navbar bg="dark" data-bs-theme="dark">
    <Container>
-    <Navbar.Brand href="#home">Pokedex<span className='red-text' >ID</span></Navbar.Brand>
+    <Navbar.Brand href="#home"><img src={logo} alt="icon" style={{ width: '30px', marginRight: '8px' }} /> Pokedex<span className='red-text' >ID</span></Navbar.Brand>
     <Navbar.Toggle />
-    <Navbar.Collapse className="justify-content-end">
-     <NavDropdown title="Langue" id="basic-nav-dropdown">
+    <Navbar.Collapse className="justify-content-end ">
+     <NavDropdown title="Langue" id="basic-nav-dropdown" className='couleur-text'>
       <NavDropdown.Item href="#action/3.1">Fr</NavDropdown.Item>
       <NavDropdown.Item href="#action/3.2">
        Another action
