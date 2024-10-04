c47113b35e387a04756604761767dc92016942dc
hitochix06 <pachara.philibert@hotmail.fr>
2024-10-04 15:41:06 +0200

    refactor: Update Card component with lazy loading for images and pokemon type icons

--- src/assets/pokemonTypelcons.js
+++ src/assets/pokemonTypelcons.js
@@ -1,6 +1,44 @@
 import fireIcon from "./type/fire.svg";
+import waterIcon from "./type/water.svg";
+import bugIcon from "./type/bug.svg";
+import normalIcon from "./type/normal.svg";
+import poisonIcon from "./type/poison.svg";
+import psychicIcon from "./type/psychic.svg";
+import rockIcon from "./type/rock.svg";
+import ghostIcon from "./type/ghost.svg";
+import steelIcon from "./type/steel.svg";
+import darkIcon from "./type/dark.svg";
+import dragonIcon from "./type/dragon.svg";
+import electricIcon from "./type/electric.svg";
+import fairyIcon from "./type/fairy.svg";
+import fightingIcon from "./type/fighting.svg";
+import flyingIcon from "./type/flying.svg";
+import grassIcon from "./type/grass.svg";
+import groundIcon from "./type/ground.svg";
+import iceIcon from "./type/ice.svg";
+import shadowIcon from "./type/shadow.svg";
+
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   fire: fireIcon,
+  water: waterIcon,
+  bug: bugIcon,
+  normal: normalIcon,
+  poison: poisonIcon,
+  psychic: psychicIcon,
+  rock: rockIcon,
+  ghost: ghostIcon,
+  steel: steelIcon,
+  dark: darkIcon,
+  dragon: dragonIcon,
+  electric: electricIcon,
+  fairy: fairyIcon,
+  fighting: fightingIcon,
+  flying: flyingIcon,
+  grass: grassIcon,
+  ground: groundIcon,
+  ice: iceIcon,
+  shadow: shadowIcon,
+
 };
