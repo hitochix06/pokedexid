6501106946cfcf6971ea25de284f442a6bd719c9
hitochix06 <pachara.philibert@hotmail.fr>
2024-10-04 15:23:45 +0200

    refactor: Update Card component with lazy loading for images and pokemon type icons

---
+++ src/assets/pokemonTypelcons.js
@@ -0,0 +1,6 @@
+import fireIcon from "./type/fire.svg";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  fire: fireIcon,
+};
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Badge from "react-bootstrap/Badge";
-import { Row, Col, Spinner } from "react-bootstrap";
+import { Spinner } from "react-bootstrap";
 import AnimatedCard from "react-animated-3d-card";
 import pokemonColors from "../data/pokemonColors.json";
+import pokemonTypeIcons from "../assets/pokemonTypelcons";
 
 function Cards({ pokemonUrl }) {
   const [pokemon, setPokemon] = useState(null);
@@ -65,7 +65,26 @@ function Cards({ pokemonUrl }) {
       onClick={() => console.log("Carte cliquée")}
     >
       <div style={{ padding: "1rem" }}>
-        <div style={{ color: "white" }}>ID: {pokemon.id}</div>
+        <div
+          style={{
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <span>ID: {pokemon.id}</span>
+          <div>
+            {pokemon.types.map((type, index) => (
+              <img
+                key={index}
+                src={pokemonTypeIcons[type]}
+                alt={type}
+                style={{ width: "30px", height: "30px", marginLeft: "5px" }}
+              />
+            ))}
+          </div>
+        </div>
         <div className="text-center">
           <h3 style={{ textTransform: "uppercase", color: "white" }}>
             {pokemon.name}
@@ -75,21 +94,6 @@ function Cards({ pokemonUrl }) {
             alt={pokemon.name}
             style={{ width: "100%", height: "auto" }}
           />
-          <Row>
-            {pokemon.types.map((type, index) => (
-              <Col key={index}>
-                <Badge
-                  pill
-                  style={{
-                    backgroundColor: pokemonColors[type] || "gray",
-                    color: "white",
-                  }}
-                >
-                  {type}
-                </Badge>
-              </Col>
-            ))}
-          </Row>
         </div>
       </div>
     </AnimatedCard>
