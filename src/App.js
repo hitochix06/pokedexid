f1da4d4266876ce6d5f6b4c6b0c52361d261aba0
hitochix06 <pachara.philibert@hotmail.fr>
2024-10-06 12:21:42 +0200

    refactor: Add lazy loading for images in PokemonListe and Card components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import React, { useState } from "react";
 import NavBar from "./components/NavBar";
-import Search from "./components/Search";
-import Pagination from "./components/Pagination";
+
 import PokemonList from "./components/PokemonListe";
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
-  const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSearch = (term) => {
-    setSearchTerm(term);
-    setCurrentPage(1);
-  };
-
-  const handlePrevious = () => {
-    setCurrentPage((prev) => Math.max(prev - 1, 1));
-  };
-
-  const handleNext = () => {
-    setCurrentPage((prev) => prev + 1);
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
   };
 
   return (
     <div className="bg-black min-h-screen text-white">
       <NavBar />
-      <Search onSearch={handleSearch} />
-      <PokemonList currentPage={currentPage} searchTerm={searchTerm} />
-      {!searchTerm && (
-        <Pagination
-          currentPage={currentPage}
-          onPrevious={handlePrevious}
-          onNext={handleNext}
-        />
-      )}
+      <PokemonList currentPage={currentPage} onPageChange={handlePageChange} />
     </div>
   );
 }
--- src/components/PokemonListe.jsx
+++ src/components/PokemonListe.jsx
@@ -3,37 +3,27 @@ import Lottie from "lottie-react";
 import Card from "./Card";
 import animationPokeball from "../assets/animationpokeball.json";
 
-function PokemonList({ currentPage, searchTerm }) {
+function PokemonList({ currentPage, searchTerm, onPageChange }) {
   const [pokemonList, setPokemonList] = useState([]);
   const [loading, setLoading] = useState(true);
   const pokemonPerPage = 20;
+  const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+  const [noResults, setNoResults] = useState(false);
 
   useEffect(() => {
     const fetchPokemonList = async () => {
       setLoading(true);
       try {
-        let url;
-        if (searchTerm) {
-          url = `https://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`;
-        } else {
-          const offset = (currentPage - 1) * pokemonPerPage;
-          url = `https://pokeapi.co/api/v2/pokemon?limit=${pokemonPerPage}&offset=${offset}`;
-        }
-
+        const offset = (currentPage - 1) * pokemonPerPage;
+        const url = `https://pokeapi.co/api/v2/pokemon?limit=${pokemonPerPage}&offset=${offset}`;
         const response = await fetch(url);
         const data = await response.json();
-
-        if (searchTerm) {
-          setPokemonList([
-            {
-              name: data.name,
-              url: `https://pokeapi.co/api/v2/pokemon/${data.id}/`,
-            },
-          ]);
-        } else {
-          setPokemonList(data.results);
-        }
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        
+        // Ajout d'un délai de 3 secondes
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        
+        setPokemonList(data.results);
+        setFilteredPokemonList(data.results);
       } catch (error) {
         console.error("Erreur lors de la récupération des Pokémon:", error);
       } finally {
@@ -42,7 +32,20 @@ function PokemonList({ currentPage, searchTerm }) {
     };
 
     fetchPokemonList();
-  }, [currentPage, searchTerm]);
+  }, [currentPage]);
+
+  useEffect(() => {
+    if (searchTerm) {
+      const filtered = pokemonList.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredPokemonList(filtered);
+      setNoResults(filtered.length === 0);
+    } else {
+      setFilteredPokemonList(pokemonList);
+      setNoResults(false);
+    }
+  }, [searchTerm, pokemonList]);
 
   if (loading) {
     return (
@@ -60,10 +63,43 @@ function PokemonList({ currentPage, searchTerm }) {
   }
 
   return (
-    <div className="pokemon-grid">
-      {pokemonList.map((pokemon, index) => (
-        <Card key={pokemon.name} pokemonUrl={pokemon.url} index={index} />
-      ))}
+    <div className="container p-3">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 mb-4">
+        {filteredPokemonList.map((pokemon, index) => (
+          <div className="col" key={pokemon.name}>
+            <Card pokemonUrl={pokemon.url} index={index} />
+          </div>
+        ))}
+        {noResults && (
+          <div className="col-12 text-center">
+            <p>Aucun résultat trouvé.</p>
+          </div>
+        )}
+      </div>
+      <nav aria-label="Navigation des pages de Pokémon">
+        <ul className="pagination justify-content-center ">
+          <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+            <button
+              className="page-link"
+              onClick={() => onPageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              Précédent
+            </button>
+          </li>
+          <li className="page-item active">
+            <span className="page-link">Page {currentPage}</span>
+          </li>
+          <li className="page-item">
+            <button
+              className="page-link"
+              onClick={() => onPageChange(currentPage + 1)}
+            >
+              Suivant
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
