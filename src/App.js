7f24cd69aa3a9b97270671fc01055d25d6e9087c
hitochix06 <pachara.philibert@hotmail.fr>
2024-10-06 13:01:18 +0200

    refactor: Add lazy loading for search input in Search component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import NavBar from "./components/NavBar";
-
 import PokemonList from "./components/PokemonListe";
-
+import Search from "./components/Search";
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -13,6 +12,7 @@ function App() {
   return (
     <div className="bg-black min-h-screen text-white">
       <NavBar />
+      <Search />
       <PokemonList currentPage={currentPage} onPageChange={handlePageChange} />
     </div>
   );
