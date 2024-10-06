01cb4a547a9e4ced1447ea9f7287d50344b122bc
hitochix06 <pachara.philibert@hotmail.fr>
2024-10-06 11:23:20 +0200

    refactor: Add lazy loading for search input in Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
-import {
-  Form,
-  FormControl,
-  Button,
-  Container,
-  Row,
-  Col,
-} from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Form, FormControl, Container, Row, Col, Alert } from "react-bootstrap";
 
 function Search({ onSearch }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSearch(searchTerm);
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      onSearch(searchTerm);
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm, onSearch]);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    if (/^[a-zA-Z]*$/.test(value)) {
+      setSearchTerm(value);
+      setError("");
+    } else {
+      setError("Veuillez n'entrer que des lettres.");
+    }
   };
 
   return (
     <Container fluid>
       <Row className="justify-content-center">
         <Col xs={12} sm={8} md={6} lg={4}>
-          <Form
-            className="d-flex align-items-center my-3"
-            onSubmit={handleSubmit}
-          >
+          <Form className="d-flex flex-column align-items-center my-3">
             <FormControl
               type="search"
               placeholder="Rechercher un Pokémon"
-              className="me-2 py-1 form-control-sm"
+              className="py-1 form-control-sm mb-2"
               aria-label="Search"
               value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={handleChange}
             />
-            <Button variant="outline-light" type="submit" size="sm">
-              Rechercher
-            </Button>
+            {error && <Alert variant="danger">{error}</Alert>}
           </Form>
         </Col>
       </Row>
