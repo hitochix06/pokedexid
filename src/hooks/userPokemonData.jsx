1e1dc3f3e3041c207598626684cd2e97032812a5
hitochix06 <pachara.philibert@hotmail.fr>
2024-10-24 23:02:56 +0200

    refactor: Add lazy loading for images in PokemonDetail component
    This commit adds lazy loading functionality for images in the PokemonDetail component. By implementing lazy loading, the images will only be loaded when they are visible on the screen, improving the performance of the component.

--- src/hooks/userPokemonData.jsx
+++ src/hooks/userPokemonData.jsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
 import { fetchAllPokemon } from "../services/pokemonService";
 
+// Cette fonction personnalisée utilise des hooks pour gérer les données des Pokémon
 export function usePokemonData() {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState("");
   const [allPokemon, setAllPokemon] = useState([]);
   const [filteredPokemon, setFilteredPokemon] = useState([]);
 
+  // Effet pour charger tous les Pokémon au montage du composant
   useEffect(() => {
     const loadPokemon = async () => {
       try {
+        // Récupération des données de tous les Pokémon
         const data = await fetchAllPokemon();
         setAllPokemon(data);
       } catch (error) {
@@ -20,6 +23,7 @@ export function usePokemonData() {
     loadPokemon();
   }, []);
 
+  // Effet pour filtrer les Pokémon en fonction du terme de recherche
   useEffect(() => {
     const filtered = allPokemon.filter((pokemon) =>
       pokemon.name.toLowerCase().startsWith(searchTerm.toLowerCase())
@@ -28,14 +32,17 @@ export function usePokemonData() {
     setCurrentPage(1);
   }, [searchTerm, allPokemon]);
 
+  // Fonction pour gérer la recherche
   const handleSearch = (term) => {
     setSearchTerm(term);
   };
 
+  // Fonction pour gérer le changement de page
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
   };
 
+  // Retourne les états et fonctions nécessaires pour l'utilisation des données des Pokémon
   return {
     currentPage,
     filteredPokemon,
--- src/services/pokemonService.jsx
+++ src/services/pokemonService.jsx
@@ -1,3 +1,4 @@
+// Cette fonction récupère tous les Pokémon depuis l'API avec une limite de 1000
 export async function fetchAllPokemon() {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1000");
   const data = await response.json();
